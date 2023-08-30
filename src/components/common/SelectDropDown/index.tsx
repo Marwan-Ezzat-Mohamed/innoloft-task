9db0f9ed61853b5a518c7f672162e8345594ca08
Marwan-Ezzat-Mohamed <marwan.ezzat.badawi@gmail.com>
2023-08-30 23:09:21 +0300

    feat: add dropdown select

--- src/components/common/SelectDropDown/index.tsx
+++ src/components/common/SelectDropDown/index.tsx
@@ -4,28 +4,46 @@ import {
   SelectItem,
   SelectTrigger,
   SelectValue,
-} from '@/components/ui/select'
+} from "@/components/ui/select";
+import { cn } from "@/lib/utils";
+import React from "react";
 
-type SelectProps = React.ComponentProps<typeof Select>
+type SelectProps = React.ComponentProps<typeof Select>;
 interface Props extends SelectProps {
-  items: { value: string; label: string }[]
-  onChange: (value: string) => void
-  placeholder?: string
+  items: { value: string; label: string }[];
+  onChange: (value: string) => void;
+  placeholder?: string;
+  error?: boolean;
+  errorMessage?: string;
 }
 
-export function SelectDropDown({ items, value, onChange, placeholder }: Props) {
-  return (
-    <Select value={`${value}`} onValueChange={onChange}>
-      <SelectTrigger>
-        <SelectValue placeholder={placeholder} />
-      </SelectTrigger>
-      <SelectContent>
-        {items.map(({ value: val, label }) => (
-          <SelectItem key={JSON.stringify(val)} value={val}>
-            {label}
-          </SelectItem>
-        ))}
-      </SelectContent>
-    </Select>
-  )
-}
+export const SelectDropDown = React.forwardRef<HTMLButtonElement, Props>(
+  ({ items, value, onChange, placeholder, error, errorMessage }, ref) => {
+    return (
+      <div className="gap-2.5">
+        <Select value={`${value}`} onValueChange={onChange}>
+          <SelectTrigger
+            ref={ref}
+            className={cn("", {
+              "ring ring-offset-0 ring-red-500 focus:ring-2 focus:ring-red-500 focus:ring-opacity-100 focus:ring-offset-0":
+                error,
+            })}
+          >
+            <SelectValue placeholder={placeholder} />
+          </SelectTrigger>
+          <SelectContent>
+            {items.map(({ value: val, label }) => (
+              <SelectItem key={JSON.stringify(val)} value={val}>
+                {label}
+              </SelectItem>
+            ))}
+          </SelectContent>
+        </Select>
+
+        {error && <p className="text-sm text-red-500 mt-2.5">{errorMessage}</p>}
+      </div>
+    );
+  }
+);
+
+SelectDropDown.displayName = "SelectDropDown";

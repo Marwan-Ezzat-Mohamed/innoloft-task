a315a4c951dd372004668cd98b445e5977a886fb
Marwan-Ezzat-Mohamed <marwan.ezzat.badawi@gmail.com>
2023-08-30 07:27:49 +0300

    feat: add button component

---
+++ src/components/common/Button/index.tsx
@@ -0,0 +1,58 @@
+import { forwardRef } from "react";
+import { VariantProps, cva } from "class-variance-authority";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+
+const buttonVariants = cva(
+  "inline-flex items-center justify-center rounded-md text-sm  hover:opacity-90 disabled:opacity-30 font-medium transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 dark:hover:bg-slate-800 dark:hover:text-slate-100 disabled:opacity-50 dark:focus:ring-slate-400 disabled:pointer-events-none dark:focus:ring-offset-slate-900 data-[state=open]:bg-slate-100 dark:data-[state=open]:bg-slate-800",
+  {
+    variants: {
+      variant: {
+        default: "bg-primary text-white ",
+        secondary: "bg-white text-slate-gray ",
+      },
+      size: {
+        default: "h-[30px] py-1 px-2.5 text-base",
+        sm: " px-2 rounded-md text-sm py-1",
+        lg: " px-8 rounded-md text-lg py-2",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+      size: "default",
+    },
+  }
+);
+
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {
+  href?: string;
+}
+
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, children, href, variant, size, ...props }, ref) => {
+    if (href && !props.disabled) {
+      return (
+        <Link
+          href={href}
+          className={cn(buttonVariants({ variant, size, className }))}
+        >
+          {children}
+        </Link>
+      );
+    }
+    return (
+      <button
+        className={cn(buttonVariants({ variant, size, className }))}
+        ref={ref}
+        {...props}
+      >
+        {children}
+      </button>
+    );
+  }
+);
+Button.displayName = "Button";
+
+export { Button };

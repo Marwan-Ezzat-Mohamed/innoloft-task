b3dae6ee4da4e8873c091ffed532751ce889c2d1
Marwan-Ezzat-Mohamed <marwan.ezzat.badawi@gmail.com>
2023-08-30 08:08:03 +0300

    style: fix border color for product details comp

--- src/components/ProductDetails/ProductDetails.tsx
+++ src/components/ProductDetails/ProductDetails.tsx
@@ -2,6 +2,7 @@ import RibbonIcon from "@/icons/Ribbon";
 import LocationIcon from "@/icons/Location";
 import { InnoloftLogo } from "@/config";
 import Image from "next/image";
+import CompanyDetails from "../CompanyDetails/CompanyDetails";
 
 interface PropsProductDetails {
   hasUserSection: boolean;
@@ -24,7 +25,7 @@ export default function ProductDetails({
   userProfilePicture,
 }: PropsProductDetails) {
   return (
-    <div className="flex rounded-md border border-snow-white bg-white max-lg:flex-col">
+    <div className="flex rounded-md border border-ghost-white bg-white max-lg:flex-col">
       <div className="relative flex flex-col rounded-bl-md rounded-tl-md">
         <div className="absolute top-0 flex h-10 items-center gap-2.5 rounded-br-md rounded-tl-md bg-white">
           <div className="flex h-full w-10 items-center justify-center rounded-br-md rounded-tl-md bg-primary text-white">
@@ -50,41 +51,12 @@ export default function ProductDetails({
       </div>
 
       {hasUserSection && (
-        <div className="flex min-w-[33%] flex-grow flex-col gap-2.5 border-l border-snow-white p-5 max-md:px-2.5">
-          <p className="font-semibold text-gunmetal-gray">Offered By</p>
-          <img
-            src={InnoloftLogo}
-            alt="Innoloft"
-            width="200px"
-            className="mt-5"
-          />
-
-          <div className="mb-2.5 flex items-center gap-2.5">
-            <Image
-              width={64}
-              height={64}
-              className="rounded-full"
-              src={userProfilePicture}
-              alt={userName}
-            />
-
-            <div className="flex flex-col gap-1 text-sm text-slate-gray">
-              <p className="font-semibold">{`${userName}`}</p>
-              <p className="">{companyName}</p>
-            </div>
-          </div>
-
-          <div className="mt-2.5 flex gap-1 text-slate-gray">
-            <LocationIcon />
-            <p className="text-sm">{companyAddress}</p>
-          </div>
-
-          <iframe
-            src={`https://maps.google.com/maps?&hl=en&q=${companyAddress}&t=&z=14&ie=UTF8&iwloc=B&output=embed`}
-            width={"100%"}
-            height={"200px"}
-          />
-        </div>
+        <CompanyDetails
+          companyName={companyName}
+          companyAddress={companyAddress}
+          userName={userName}
+          userProfilePicture={userProfilePicture}
+        />
       )}
     </div>
   );

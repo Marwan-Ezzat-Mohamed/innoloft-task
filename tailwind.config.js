679b090d9cbe2c3cdc2a8d2e8198916613957129
Marwan-Ezzat-Mohamed <marwan.ezzat.badawi@gmail.com>
2023-08-30 03:09:31 +0300

    feat: add color palette to tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,48 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ["class"],
+  content: [
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    extend: {
+      colors: {
+        "midnight-blue": "#272E71",
+        "gunmetal-gray": "#374151",
+        "snow-white": "#FBFAF9",
+        "slate-gray": "#6B7280",
+        "cadet-gray": "#9CA3AF",
+        "ghost-white": "#E5E7EB",
+        "light-gray": "#D1D5DB",
+        primary: {
+          DEFAULT: "var(--primary)",
+        },
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: 0 },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
+};

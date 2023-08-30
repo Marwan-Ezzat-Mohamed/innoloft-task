1e8cb98c6443a5a73823b14261a4bae3ebab7646
Marwan-Ezzat-Mohamed <marwan.ezzat.badawi@gmail.com>
2023-08-30 03:05:28 +0300

    fix(imports): fixed wrong inputs in redux slices

--- src/store/slices/theme.ts
+++ src/store/slices/theme.ts
@@ -1,57 +1,58 @@
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
-import { API_BASE_URL, APP_ID } from 'config'
-import { Theme } from 'Types/theme'
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { API_BASE_URL, APP_ID } from "@/config";
+import { Theme } from "@/Types/theme";
 
 // Define a type for the slice state
 interface ThemeState {
-  theme: Theme
-  loading: boolean
-  error: string | null
+  theme: Theme;
+  loading: boolean;
+  error: string | null;
 }
 
 const initialState: ThemeState = {
   theme: {
     id: 1,
-    logo: 'https://img.innoloft.de/logo.svg',
-    mainColor: '#272e71',
+    logo: "https://img.innoloft.de/logo.svg",
+    mainColor: "#272e71",
     hasUserSection: true,
   },
   error: null,
   loading: true,
-}
+};
 
-export const fetchTheme = createAsyncThunk('fetchTheme', async () => {
-  const response = await fetch(`${API_BASE_URL}/configuration/${APP_ID}/`)
+export const fetchTheme = createAsyncThunk("fetchTheme", async () => {
+  const response = await fetch(`${API_BASE_URL}/configuration/${APP_ID}/`);
 
   if (!response.ok) {
-    throw new Error('Error fetching theme configuration')
+    throw new Error("Error fetching theme configuration");
   }
 
-  const data = await response.json()
+  const data = await response.json();
 
-  return data
-})
+  return data;
+});
 
 export const themeSlice = createSlice({
-  name: 'theme',
+  name: "theme",
   initialState,
   reducers: {},
-  extraReducers: builder => {
+  extraReducers: (builder) => {
     builder
-      .addCase(fetchTheme.pending, state => {
-        state.loading = true
-        state.error = null
+      .addCase(fetchTheme.pending, (state) => {
+        state.loading = true;
+        state.error = null;
       })
       .addCase(fetchTheme.fulfilled, (state, action) => {
-        state.loading = false
-        state.theme = action.payload
+        state.loading = false;
+        state.theme = action.payload;
       })
       .addCase(fetchTheme.rejected, (state, action) => {
-        state.loading = false
+        state.loading = false;
         state.error =
-          action.error.message ?? 'Something went wrong while fetching theme config'
-      })
+          action.error.message ??
+          "Something went wrong while fetching theme config";
+      });
   },
-})
+});
 
-export default themeSlice.reducer
+export default themeSlice.reducer;
